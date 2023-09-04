3ed7098f5881576f72413c556f99c207c41c6c2d
TaufiqOdhi <taufiq.odhi@gmail.com>
2023-09-04 10:16:29 +0700

    change file input output to minio, for no prune

--- no_prune/main.py
+++ no_prune/main.py
@@ -1,18 +1,22 @@
-import sys
-import os
-import datetime
-import io
+from io import BytesIO
 import torch
+import os
 import numpy as np
+from pathlib import Path
 from PIL import Image
 from model import Generator
-from compress import compress_binary, recovery_binary, recovery_srgan
+from compress import recovery_srgan
 from config import DEVICE
-
+from minio_connection import minio_client, bucket_name
 
 if __name__ == "__main__":
     input_filepath = f'input_files/{os.getenv("FILENAME")}'
     output_filepath = f"output_files/{os.getenv('FILENAME')}"
+    
+    input_filepath = minio_client.get_object(
+        bucket_name=bucket_name,
+        object_name=input_filepath,
+    )
     
     gen = Generator()
     gen.load_state_dict(torch.load('gen.pth.tar')["state_dict"])
@@ -22,7 +26,15 @@ if __name__ == "__main__":
     # input_image = compress_binary(input_image)
     # input_image = recovery_binary(input_image)
     input_image = recovery_srgan(img=input_image, gen=gen)
-    Image.fromarray(input_image).save(output_filepath)
+    output_bytes = BytesIO()
+    Image.fromarray(input_image).save(output_bytes, format='PNG')
+    output_bytes = output_bytes.getvalue()
+    minio_client.put_object(
+        bucket_name=bucket_name,
+        object_name=output_filepath,
+        data=BytesIO(output_bytes),
+        length=len(output_bytes),
+    )
 
     print(output_filepath)    
     # print(image)
