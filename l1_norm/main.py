5d35ceaf38ea9233ced1b4cab766fd5b53696d32
Taufiq Odhi <taufiq.odhi@gmail.com>
2023-11-24 15:43:09 +0700

    menambah status process & message process for pruned model

--- l1_norm/main.py
+++ l1_norm/main.py
@@ -20,37 +20,48 @@ def srgan(filename: str, ip_host: str, start_timestamp: str, prune_amount: str):
         bucket_name=bucket_name,
         object_name=input_filepath,
     )
-    
-    gen = GeneratorPruned(prune_amount=float(prune_amount)/100)
-    gen.load_state_dict(torch.load(f'l1_norm/checkpoints/{float(prune_amount)/100}/gen.pth.tar')["state_dict"])
-    gen.eval().to(DEVICE)
 
-    input_image = np.asarray(Image.open(input_filepath))
-    # input_image = compress_binary(input_image)
-    # input_image = recovery_binary(input_image)
-    input_image = recovery_srgan(img=input_image, gen=gen)
-    output_bytes = BytesIO()
-    Image.fromarray(input_image).save(output_bytes, format='PNG')
-    output_bytes = output_bytes.getvalue()
-    minio_client.put_object(
-        bucket_name=bucket_name,
-        object_name=output_filepath,
-        data=BytesIO(output_bytes),
-        length=len(output_bytes),
-    )
+    try:
+        gen = GeneratorPruned(prune_amount=float(prune_amount)/100)
+        gen.load_state_dict(torch.load(f'l1_norm/checkpoints/{float(prune_amount)/100}/gen.pth.tar')["state_dict"])
+        gen.eval().to(DEVICE)
 
-    vram_log_path = os.path.splitext(filename)[0] + "".join(random.sample(string.ascii_letters, 3)) + '.txt'
-    requests.post(url=f'http://{ip_host}:8000/vram_logs',
-                  data=dict(filename=vram_log_path,
-                            image_filename=filename,
-                            start_timestamp=start_timestamp,
-                            tipe_model=f"l1_norm_{prune_amount}",
-                            ip_host_manager=os.getenv('MANAGER_HOST', 'localhost'),
-                        )
+        input_image = np.asarray(Image.open(input_filepath))
+        # input_image = compress_binary(input_image)
+        # input_image = recovery_binary(input_image)
+        input_image = recovery_srgan(img=input_image, gen=gen)
+        output_bytes = BytesIO()
+        Image.fromarray(input_image).save(output_bytes, format='PNG')
+        output_bytes = output_bytes.getvalue()
+        minio_client.put_object(
+            bucket_name=bucket_name,
+            object_name=output_filepath,
+            data=BytesIO(output_bytes),
+            length=len(output_bytes),
+        )
+
+        vram_log_path = os.path.splitext(filename)[0] + "".join(random.sample(string.ascii_letters, 3)) + '.txt'
+        requests.post(url=f'http://{ip_host}:8000/vram_logs',
+            data=dict(filename=vram_log_path,
+                        image_filename=filename,
+                        start_timestamp=start_timestamp,
+                        tipe_model=f"l1_norm_{prune_amount}",
+                        ip_host_manager=os.getenv('MANAGER_HOST', 'localhost'),
+                    )
+            )
+
+        print(output_filepath)    
+        # print(image)
+    except RuntimeError as e:
+        requests.post(url=f'http://{ip_host}:8000/vram_logs',
+            data=dict(filename='-',
+                    image_filename=filename,
+                    start_timestamp=start_timestamp,
+                    tipe_model=f"l1_norm_{prune_amount}",
+                    ip_host_manager=os.getenv('MANAGER_HOST', 'localhost'),
+                    status_process='Fail', message_process=e.__str__(),
                 )
-
-    print(output_filepath)    
-    # print(image)
+        )
 
 
 if __name__ == "__main__":
